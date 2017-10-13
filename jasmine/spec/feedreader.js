bf4670f5eb36d286a02db417ffd7f5e575227cac
tarfa <tarfa.mansour@gmail.com>
2017-10-13 15:45:52 +0300

    Submit
    Submit

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -35,12 +35,6 @@ $(function() {
 
         /* TestCase 4: The menu should be hidden by default
          */
-        /* TODO: Write a test that ensures the menu element is
-         * hidden by default. You'll have to analyze the HTML and
-         * the CSS to determine how we're performing the
-         * hiding/showing of the menu element.
-         */
-
     describe('The menu', function() {
       it('The menu item should  be hidden  ...', function() {
        var bodyConnten = $("body");
@@ -48,12 +42,9 @@ $(function() {
          expect(bodyConnten.hasClass("menu-hidden")).toBe(true);
 
       });
-
-         /* TODO: Write a test that ensures the menu changes
-          * visibility when the menu icon is clicked. This test
-          * should have two expectations: does the menu display when
-          * clicked and does it hide when clicked again.
-          */
+      /* TestCase 5: The menu should display when
+      clicked and does it hide when clicked again.
+      */
           it('The menu icon is clicked ...', function() {
            var bodyConnten = $("body");
            var menu = $(".menu-icon-link");
@@ -68,14 +59,10 @@ $(function() {
 
 
                   });//End menu
-    /* TODO: Write a new test suite named "Initial Entries" */
 
-        /* TODO: Write a test that ensures when the loadFeed
-         * function is called and completes its work, there is at least
-         * a single .entry element within the .feed container.
-         * Remember, loadFeed() is asynchronous so this test will require
-         * the use of Jasmine's beforeEach and asynchronous done() function.
-         */
+        /* TestCase 6: Initial Entries should be created
+                  */
+
          describe('Initial Entries should be created ...', function() {
            it('Ensures when the loadFeed is completed and loaded  ...', function() {
               beforEach(function(done){
@@ -85,11 +72,7 @@ $(function() {
             });
 
 
-    /* TODO: Write a new test suite named "New Feed Selection" */
-
-        /* TODO: Write a test that ensures when a new feed is loaded
-         * by the loadFeed function that the content actually changes.
-         * Remember, loadFeed() is asynchronous.
+    /* TestCase 7: Ensures when a new feed is loaded
          */
             loadFeed(1, function() {
                 var Afterloaded = $('.feed').html();
